176af55b95a8f68c4731e1c3f6ffe21136b3b469
Rawan <rawansalameh160596@gmail.com>
2020-04-23 21:08:24 +0300

    Adding project1

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,16 @@
+package com.jetbrains;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+
+    }
+
